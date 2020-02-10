f5f07f566cf47e7a2bd8b98c4d1de1c6a1cf4495
Şəmistan Hüseynov <shamistan1999@gmail.com>
2020-02-10 18:53:02 +0400

    Sum

---
+++ src/main/java/app/Calculator.java
@@ -0,0 +1,27 @@
+package app;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int sum;
+        InputStream in=System.in;
+        PrintStream output = System.out;
+        Scanner sc= new Scanner(in);
+        output.print("Please enter the first number: ");
+        a=sc.nextInt();
+        output.print("Please enter the second number: ");
+        b=sc.nextInt();
+        sum=a+b;
+        output.println("it is your sum: "+sum);
+
+
+
+
+    }
+}
