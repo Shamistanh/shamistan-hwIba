ffb9026dd1045f2a24c84fe35a957b4d6abb329d
Şəmistan Hüseynov <shamistan1999@gmail.com>
2020-03-18 16:34:21 +0400

    18m

--- src/main/java/March18/Warmup.java
+++ src/main/java/March18/Warmup.java
@@ -9,16 +9,16 @@ public class Warmup {
 
     static int calc(String origin){
         int interval=0;
-        int cnt=0;
+        int shoes=0;
         for (int i = 0; i <origin.length() ; i++) {
             switch (origin.charAt(i)){
                 case 'L': interval++; break;
                 case 'R': interval--; break;
             }
             if(interval==0 && i>0){
-                cnt++;
+                shoes++;
             }
         }
-        return cnt;
+        return shoes;
     }
 }
